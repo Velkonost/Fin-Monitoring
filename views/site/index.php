c7f92dda7c3280ab7cfcf310385143a717efa987
Artem <klimenkocpp@icloud.com>
2017-07-10 12:24:06 +0600

    added metals

--- views/site/index.php
+++ views/site/index.php
@@ -20,30 +20,43 @@ $today = date("d.m.y");
 <style type="text/css">
     /* 58.5 */
 
-    table {
+    .inputTable {
        margin: 0 auto; 
     text-align: left;
-    
+
 
     }
 
-    td {
+    .inputTable td {
         background-color: #fff8ca;
-
-    }
-
-    td{
         padding-left: 10px;
         padding-right: 10px;
+    }
+    .types td img {
+        max-width: 75px;
+        min-width: 75px;
+
+        min-height: 75px;
+        max-height: 75px;
+
+        
+    }
+    .types td{
+        font-size: 10px;
+        padding-bottom: 20px;
+    }
+  
+    h3{
+        color: black;
+        font-weight: normal;
+        margin-bottom: 0px;
     }
 
 </style>
 
- <table style="border-collapse: separate; border-spacing: 2px;">
+ <table class="inputTable" style="border-collapse: separate; border-spacing: 2px;margin-left:-3%">
 
     <tbody  style="min-width: 1170px">
-            
-        
                 <tr class='hidden-row'>
                     <td style="max-width: 58.5px; min-width: 58.5px;text-align: center; padding: 0"><?=$today?></td>
                     <td style="max-width: 58.5px; min-width: 58.5px;text-align: center; padding: 0"><?=date('h:i');?></td>
@@ -57,12 +70,150 @@ $today = date("d.m.y");
                     <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style' => 'width:117px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
                     <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style' => 'width:117px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
                     <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style' => 'width:117px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
-
-                  
-                    
                 </tr>
-            
     </tbody>
+</table>
+
+<table class="types">
+    <caption><h3>Металлы</h3></caption>
+    <tr>
+        <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+         <td>
+            <img src="../web/img/metall.jpeg">
+            <br>Металл
+            <br>Бронза
+            <br>Чистая
+        </td>
+    </tr>
+
+
 </table>
 
 <?= Html::submitButton('Добавить', ['id'=>'future', 'name' => 'button_save']) ?>
