a70c8a77691d175a3d7c24324a15279b76cd8055
Artem <klimenkocpp@icloud.com>
2017-07-10 11:36:19 +0600

    123

--- views/site/index.php
+++ views/site/index.php
@@ -13,24 +13,36 @@ $this->title = 'My Yii Application';
 ?>
 <?php $f = ActiveForm::begin()?>
 
+<style type="text/css">
+    /* 58.5 */
+    td {
+        background-color: #f7ed5b;
+       /*  padding-left: 10px;
+        padding-right: 10px; */
+    }
+
+</style>
+
  <table class="Russia" style="border-collapse: separate; border-spacing: 1px;">
 
-    <tbody class="hidden_table">
+    <tbody class="hidden_table" style="width: 1170px">
             
         
                 <tr class='hidden-row'>
-                    <td><span>07.07.17</span></td>
-                    <td><span>14:36</span></td>
-                    <td><?=$f->field($form, 'article[]')->dropDownList($allarticles, ['id' => "selectName$i", 'style'=>'width:205px; margin-left:5px;','options' => ['0'=>['selected'=>true]]])->label('');?></td>
-                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style'=>' width: 200px; margin-left:5px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
+                    <td style="max-width: 58.5px; min-width: 58.5px;text-align: center;">07.07.17</td>
+                    <td style="max-width: 58.5px; min-width: 58.5px;text-align: center;">14:36</td>
+                    <td><?=$f->field($form, 'article[]')->dropDownList($allarticles, ['id' => "selectName$i", 'style' => 'width:175.5px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
+                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style' => 'width:175.5px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
+                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style' => 'width:175.5px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
 
-                    <td><?=$f->field($form, 'ss[]')->textInput(['style'=>'width:98%' ,'value' =>'0', 'type'=>'number', 'min' => '0'])->label('')?></td>
-                    <td><?= $f->field($form, 'ms[]')->textInput(['style'=>'width:98%' ,'value' =>'0', 'type'=>'number', 'min' => '0'])->label('')?></td>
-                    <td><?= $f->field($form, 'ls[]')->textInput(['style'=>'width:98%' , 'value' =>'0','type'=>'number', 'min' => '0'])->label('')?></td>
-                    <td><?= $f->field($form, 'xls[]')->textInput(['style'=>'width:98%' ,'value' =>'0', 'type'=>'number', 'min' => '0'])->label('')?></td>
-                    <td><?= $f->field($form, 'xxls[]')->textInput(['style'=>'width:98%' , 'value' =>'0','type'=>'number', 'min' => '0'])->label('')?></td>
-                    <td><?= $f->field($form, 'xxxls[]')->textInput(['style'=>'width:98%' , 'value' =>'0','type'=>'number', 'min' => '0'])->label('')?></td>
-                    <td><?= $f->field($form, 'prices[]')->textInput(['style'=>'width:98%' , 'value' =>'0','type'=>'number', 'min' => '0'])->label('')?></td>
+                    <td><?=$f->field($form, 'ss[]')->textInput(['value' =>'0', 'type'=>'number', 'min' => '0'])->label('')?></td>
+                    <td><?= $f->field($form, 'ms[]')->textInput(['value' =>'0', 'type'=>'number', 'min' => '0'])->label('')?></td>
+
+                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
+                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
+                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
+
+                  
                     
                 </tr>
             
