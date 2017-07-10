8e29bf4ed8a6c6c3cf19c8dc3d37632d9ec62d8a
Artem <klimenkocpp@icloud.com>
2017-07-10 12:06:34 +0600

    321

--- views/site/index.php
+++ views/site/index.php
@@ -10,37 +10,53 @@ use yii\web\helpers\CHtml;
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
+
+date_default_timezone_set('UTC + 3');
+$today = date("d.m.y");
+
 ?>
 <?php $f = ActiveForm::begin()?>
 
 <style type="text/css">
     /* 58.5 */
+
+    table {
+       margin: 0 auto; 
+    text-align: left;
+    
+
+    }
+
     td {
-        background-color: #f7ed5b;
-       /*  padding-left: 10px;
-        padding-right: 10px; */
+        background-color: #fff8ca;
+
+    }
+
+    td{
+        padding-left: 10px;
+        padding-right: 10px;
     }
 
 </style>
 
- <table class="Russia" style="border-collapse: separate; border-spacing: 1px;">
+ <table style="border-collapse: separate; border-spacing: 2px;">
 
-    <tbody class="hidden_table" style="width: 1170px">
+    <tbody  style="min-width: 1170px">
             
         
                 <tr class='hidden-row'>
-                    <td style="max-width: 58.5px; min-width: 58.5px;text-align: center;">07.07.17</td>
-                    <td style="max-width: 58.5px; min-width: 58.5px;text-align: center;">14:36</td>
+                    <td style="max-width: 58.5px; min-width: 58.5px;text-align: center; padding: 0"><?=$today?></td>
+                    <td style="max-width: 58.5px; min-width: 58.5px;text-align: center; padding: 0"><?=date('h:i');?></td>
                     <td><?=$f->field($form, 'article[]')->dropDownList($allarticles, ['id' => "selectName$i", 'style' => 'width:175.5px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
                     <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style' => 'width:175.5px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
-                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style' => 'width:175.5px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
+                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style' => 'width:117px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
 
-                    <td><?=$f->field($form, 'ss[]')->textInput(['value' =>'0', 'type'=>'number', 'min' => '0'])->label('')?></td>
-                    <td><?= $f->field($form, 'ms[]')->textInput(['value' =>'0', 'type'=>'number', 'min' => '0'])->label('')?></td>
+                    <td><?=$f->field($form, 'ss[]')->textInput(['value' =>'0', 'style' => 'width:58.5px', 'type'=>'number', 'min' => '0'])->label('')?></td>
+                    <td><?= $f->field($form, 'ms[]')->textInput(['value' =>'0', 'style' => 'width:58.5px', 'type'=>'number', 'min' => '0'])->label('')?></td>
 
-                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
-                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
-                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
+                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style' => 'width:117px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
+                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style' => 'width:117px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
+                    <td><?=$f->field($form, 'names[]')->dropDownList($allclothes, ['id' => "selectArticle$i", 'style' => 'width:117px', 'options' => ['0'=>['selected'=>true]]])->label('');?></td>
 
                   
                     
